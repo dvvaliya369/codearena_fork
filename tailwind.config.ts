7d0fce822250ac0f47fa7c74147fd2ba0293d627
Sam Selikoff <sam.selikoff@gmail.com>
2024-12-02 16:02:28 -0500

    Add shad button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
 import colors from "tailwindcss/colors";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { gray, blue, ...rest } = colors;
 
 export default {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,21 +16,68 @@ export default {
     colors: {
       ...rest,
       gray: {
-        100: "#DADADA",
-        200: "#FAF8F5",
-        300: "#F6F2EC",
-        500: "#898989",
-        900: "#181818",
+        "100": "#DADADA",
+        "200": "#FAF8F5",
+        "300": "#F6F2EC",
+        "500": "#898989",
+        "900": "#181818",
       },
       blue: {
-        500: "#4845FE",
+        "500": "#4845FE",
       },
     },
     extend: {
       fontFamily: {
         sans: ["var(--font-tektur)"],
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
